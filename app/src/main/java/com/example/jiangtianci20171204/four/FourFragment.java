4ad768ca23d625542c987b174635b1c3174c60c6
jiangtianci <31463317+jiangtianci@users.noreply.github.com>
2017-12-05 14:43:11 +0800

    Initial commit

---
+++ app/src/main/java/com/example/jiangtianci20171204/four/FourFragment.java
@@ -0,0 +1,20 @@
+package com.example.jiangtianci20171204.four;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by dell on 2017/12/2.
+ */
+
+public class FourFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+}
